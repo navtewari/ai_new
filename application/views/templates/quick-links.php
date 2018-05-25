5102a87c18dc37e91ff6840bab0a0034f8d2a487
Dr. Nitin Deepak <nitin.d12@gmail.com>
2018-05-25 13:49:10 +0530

    updates
    updates

--- application/views/templates/quick-links.php
+++ application/views/templates/quick-links.php
@@ -2,14 +2,14 @@
                                                 <aside class="widget widget_categories">
                                                     <h2 class="widget-title">QUICK LINKS</h2>	
                                                     <ul>
-                                                        <li><a href="#">Why Amrapali?</a></li>
+                                                        <li><a href="<?php echo site_url('Agi/whyAmrapali');?>">Why Amrapali?</a></li>
                                                         <li><a href="#"><i class="fa fa-cog fa-spin"></i> Apply Online</a></li>
-                                                        <li><a href="#">Admission Procedure</a></li>
-                                                        <li><a href="#">Fee Structures</a></li>
-                                                        <li><a href="#">Training &amp; Placement</a></li>
-                                                        <li><a href="#">Infrastructure</a></li>
-                                                        <li><a href="#">Facilities</a></li>
-                                                        <li><a href="#">Library</a></li>
+                                                        <li><a href="<?php echo site_url('Agi/admissionProcedure');?>">Admission Procedure</a></li>
+                                                        <li><a href="<?php echo site_url('Agi/feeStructure');?>">Fee Structures</a></li>
+                                                        <li><a href="<?php echo site_url('Agi/trainingAndPlacement');?>">Training &amp; Placement</a></li>
+                                                        <li><a href="<?php echo site_url('Agi/infrastructure');?>">Infrastructure</a></li>
+                                                        <li><a href="<?php echo site_url('Agi/infrastructure');?>">Facilities</a></li>
+                                                        <li><a href="<?php echo site_url('Agi/library');?>">Library</a></li>
                                                         <li><a href="#">Vision 2020</a></li>                                                        
                                                         <li><a href="#">Contact Us</a></li>
                                                     </ul>
