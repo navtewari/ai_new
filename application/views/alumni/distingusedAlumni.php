ce368c93b645f5b52244c4f2b01bff950253e1ac
Dr. Naveen Tewari <navtewari@gmail.com>
2018-05-29 10:10:30 +0530

    up
    up

--- application/views/alumni/distingusedAlumni.php
+++ application/views/alumni/distingusedAlumni.php
@@ -1,56 +1,4 @@
-<style>    
-    .profile{
-        position: relative;
-        float:left;
-        width:250px;
-        height:300px;
-        background: #ffffe6;
-        box-shadow: 1px 1px 1px #e6e6e4;
-        padding:10px;
-        margin:10px;
-    }
-    .pname{
-        color: #08C;
-        font-weight:400;
-        line-height: 20px;
-        margin-top: 0;
-        margin-bottom:0px;
-    }
-    .cname{
-        color: #ff6600;
-        font-weight:400;
-        line-height: 20px;
-         margin-top: 0;
-        margin-bottom:0px;
-    }
-    .presume{
-        color: #c7254e;
-        font-weight:400;
-    }
-    .cross{
-        position: absolute;
-        top: 0px;
-    }
 
-    .enable{
-        position: absolute;
-        top: 0px;
-        right:0px;
-    }
-
-    .disable{
-        position: absolute;
-        top: 0px;
-        right:0px;
-    }
-    h5{        
-        color: #c7254e;        
-        letter-spacing: .02em;
-        font-weight: 400;
-        font-size: 1.4em;     
-        text-shadow: 1px 1px 1px #fff;
-    }
-</style>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">  
 <body>
     <div id="st-container" class="st-container">
@@ -94,7 +42,7 @@
                                                 <section>
                                                 <blockquote>They were here, Yesterday. Today, they are our Brand Ambassadors in Top Organizations.</blockquote>               
                                                 <?php foreach ($fac_profile as $item) { ?>
-                                                    <div class="col-md-4 col-sm-4 col-xs-12">
+                                                    <div class="col-md-4 col-sm-4 col-xs-12" style="max-height:389px;min-height:389px; overflow:hidden;">
                                                         <figure style="background: #f3f3f3;margin-top: 2em;padding:.5em;">
                                                             <div class="css-team-avatar" align="center">               
                                                                 <img class="media-object img-responsive" src="<?php echo base_url('assets/alumniPic/' . $item->pic); ?>" alt="Amrapali alumni - <?php echo $item->name; ?>" style="max-width: 140px; height:150px;">             
@@ -107,7 +55,7 @@
                                                             </h3>
                                                             </div>
                                                         </figure>
-                                                    </div>                                                    
+                                                    </div> 
                                                 <?php } ?>
                                                 </section>                                                 
                                             </div><!-- /.entry-content -->
