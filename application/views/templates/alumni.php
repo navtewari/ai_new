f5ace5225b42311ad015e2a176577acabb19fa44
Dr. Naveen Tewari <navtewari@gmail.com>
2018-05-30 21:26:01 +0530

    up
    up

--- application/views/templates/alumni.php
+++ application/views/templates/alumni.php
@@ -1,47 +1,101 @@
+<style>
+    blockquote p{
+        font-size:14px;
+        line-height: 22px;
+    }
+
+    blockquote img{
+        max-width:100px;
+        margin: 0 10px;
+        padding:0;
+        border: 2px solid #bbbbbb;
+        border-radius: 5px;
+    }
+</style>
+
 <div class="row">
                                         <?php
                                         $data = array(
                                             array(
-                                                'photo' => 'manishPodiyal.jpg',
-                                                'name' => 'Manish Podiyal',
-                                                'where' => 'Senior Software Enginer, SmartData Enterprises, Chandigarh (Alumnus, BCA &amp; MCA)',
-                                                'text_' => 'The kind of exposure we got from our Institute was Alumnus, MCA phenomenal. Education makes the base stronger and I am thankful to AIMCA for providing great quality education which has played a very important role in my professional growth. All I can say is that the Institute gave us third eye to see how world is moving and where we can position ourselves globally.'
+                                                'photo' => 'AnsulSanwal.jpg',
+                                                'name' => 'Ansul Sanwal',
+                                                'where' => 'Resident Manager, Reethi Faru Resort, Maldives(Alumnus, HM)',
+                                                'text_' => 'Do you know what it feels being where I am today.
+                                                    <p> Great, Simply gorgeous and great! Every little thing counts and AIHM has given me big richness of life through the undefeatable trait backed by knowledge. I owe every bit of my success to the great mentors of Amrapali who have helped to shape and design my career, MY LIFE!</p>
+                                                    <p> A big thanks to all my teachers. The footsteps I follow were the hard work of my great teachers. '
                                             ),
                                             array(
-                                                'photo' => 'shailenderBhatt.jpg',
-                                                'name' => 'Shelendra Bhatt',
-                                                'where' => 'Portfolio Analyst, Microsoft Corp. (Alumnus, MBA)',
-                                                'text_' => 'I chose to study at Amrapali Group of Institutes because I wanted to study at the highest level nearby my region & stretch my mind. I felt that college environment was very friendly starting from teachers to students. I cannot ignore the highly cooperative administration. I would say that studying at Amrapali Institute has been one of the best experiences of my life. I have no hesitation in recommending people to come and study at this prestigious College to build a great future.'
+                                                'photo' => 'JagdishTiwari.jpg',
+                                                'name' => 'Jagdish Tiwari',
+                                                'where' => 'Food & Beverage Manager, Etihad Airways, UAE. (Alumnus, HM)',
+                                                'text_' => 'As the years of knowledge and learning unfold I could feel the strength AIHM has in helping me carve a niche of my choice.
+                                                    <p> The day I joined AIHM I was not sure about my career horizons but in second year of my study itself I was able to decide about my future goals, for which I am thankful to my mentors at institute.</p>
+                                                    <p> My words speak through my craft as I receive recognition in this field and grow. A humble thanks to Amrapali for kindling the zest to rise in me. Thank you respected mentors! </p>'
                                             ),
                                             array(
-                                                'photo' => 'vineetPant.jpg',
-                                                'name' => 'Vineet Pant',
-                                                'where' => 'Senior Software Enginer, Aricent Technologies Bangalore (Alumnus, MCA)',
-                                                'text_' => 'I chose to study at Amrapali Group of Institutes because I wanted to study at the highest level nearby my region & stretch my mind. I felt that college environment was very friendly starting from teachers to students. I cannot ignore the highly cooperative administration. I would say that studying at Amrapali Institute has been one of the best experiences of my life. I have no hesitation in recommending people to come and study at this prestigious College to build a great future.'
+                                                'photo' => 'LalitMishra.jpg',
+                                                'name' => 'Lalit Mishra',
+                                                'where' => 'Astt. Beverage Manager, Four Seasons Hotel- Bahrain(Alumnus, HM)',
+                                                'text_' => 'After four amazing years at AIHM, I felt confident, motivated and my carrier had a focused direction, and most importantly I had a strong portfolio. That strong portfolio helped me get accepted.
+                                                    <p> It was indeed a learning exposure and a sound practical knowledge which was imparted to me during my study years at Amrapali. I was chiseled by mentors and the technical skilled learnt polished my profile towards a strong portfolio.</p>
+                                                    <p> What does Four Season mean to me now is the beginning of a long journey ahead.  '
                                             ),
                                             array(
-                                                'photo' => 'sanjayDhyani.jpg',
-                                                'name' => 'Sanjay Dhyani',
-                                                'where' => 'Customer Engineer- Server &amp; Storage, Hewlett Packard Enterprise, (Alumnus B.Tech)',
-                                                'text_' => 'I spent four years studying at Amrapali and this was an absolutely unforgettable period in my life. I found welcoming classmates, engaged faculty, and a built-in community at AITS. My study experiences promoted close bonds, friendships, memories, and context for all we were learning in our classes back home.'
+                                                'photo' => 'ManishMishra.jpg',
+                                                'name' => 'Manish Mishra',
+                                                'where' => 'Sales Manager, Asia Petrochemicals Dubai (Alumnus HM)',
+                                                'text_' => 'I would describe Amrapali as exciting and dynamic. The best thing about pursuing MBA program is the number of additional opportunities that are available. I was initially attracted by the schools reputation, and my career goals.
+                                                    <p> To any new student coming to Amrapali, I would say this: Throw yourself at every opportunity that comes your way. Opportunities make all the difference between getting what you want.</p>'
                                             ),
                                             array(
-                                                'photo' => 'hemantRawat.jpg',
-                                                'name' => 'Hemant Rawat',
-                                                'where' => 'Engineer, Client Services Gemalto, (Alumnus B.Tech)',
-                                                'text_' => 'I am thankful to Amrapali for what I am today. Words cannot describe everything that I learnt here. Discipline, fun, confidence, taking fearless decisions and enjoying each moment of the day were few of the salient features of my stay at the institute.'
+                                                'photo' => 'BhagwatiKandpal.jpg',
+                                                'name' => 'Bhagwati Kandpal',
+                                                'where' => 'HR Executive, EQS Group (Alumnus MBA)',
+                                                'text_' => 'It was a great time we spent in Amrapali Group of Institutes while pursuing MBA. The institute has very wonderful faculty and they are very cooperative and campus environment is very positive. The department has made the sincere efforts to bring the best out of us. The days spent at Amrapali are the moments for the life time. '
                                             ),
                                             array(
-                                                'photo' => 'TanujaKabdawal.jpg',
-                                                'name' => 'Tanuja Kabdawal',
-                                                'where' => 'The Leela Palace,  Bengaluru (Alumnus, HM)',
-                                                'text_' => 'Everyone says college life is the best part of the life and I came to know this when I joined AIHM. This is the place where I learned a lot not only related to the syllabus but also about how to be good person. The teachers are very helpful and they always supported and guided me on the right path and helped me in taking some important decisions for my future. It was the best part of my life and wish everyone have such a nice experience.'
+                                                'photo' => 'TusharSaluja.jpg',
+                                                'name' => 'Tushar Saluja',
+                                                'where' => 'Asst. Manager-Trading Auctions, Agri Bazaar (Alumnus, MBA)',
+                                                'text_' => 'Nothing can be as satisfying as writing a testimony for your own alma mater. Faculty of Commerce & Business Management at Amrapali is one of the best and is known for its excellence. It has all the best facilities under one roof - state of infrastructure, erudite faculty, professional teachers, well stocked libraries, good computer labs, sports area, and hostels with all amenities. I am proud to be an alumnus of Amrapali.'
                                             ),
                                             array(
-                                                'photo' => 'ChandraShekhar.jpg',
-                                                'name' => 'Chandra Shekhar',
-                                                'where' => 'J W Marriott, New Delhi (Alumnus, HM)',
-                                                'text_' => 'A new academic year begins with new energy and vibrant faces on the campus. I want to give a small  message to every individual who joins AIHM  that here you will get ample opportunities to explore and develop yourself. The priceless knowledge that I imbibed in AIHM  is my greatest asset. Thanks a lot to all my “GURUS”. This college has a mesmerizing environment, that one feels comfortable the moment he/she enters the college.'
+                                                'photo' => 'SavitaBisht.jpg',
+                                                'name' => 'Savita Bisht',
+                                                'where' => 'Assistant System Engineer, TCS  (Alumnus, MCA)',
+                                                'text_' => 'I feel the kind of exposure we got from our Institute was exceptional.                                                 
+                                                    <p>The exposure was not only limited to the boundaries of our Institute but was global too which helped us realize and practice new technologies and processes. The expert ses s ions and training schedules gave us very good insight. All I can say is that the Institute gave me new perspective towards my career. As I take new strides in my career, I always remember my institute with gratitude. It gave me a strong foundation of concepts that come very handy in my corporate life. </p>'
+                                            ),
+                                            array(
+                                                'photo' => 'AnkitJoshi.jpg',
+                                                'name' => 'Ankit Joshi',
+                                                'where' => 'Team Lead, Valtech (Alumnus, MCA)',
+                                                'text_' => '"Amrapali" gave me the platform to mould my future towards my desirable dream. It helped me in every possible way in taking out my possibilities for countering the practical professional world. The faculty members were very helpful throughout my MCA academic journey.                                                    
+                                                    <p>I am thankful to faculty members for providing the base and initial step in entering the professional world through placement support, which helped me in taking further growing steps in my life.</p>'
+                                            ),
+                                            array(
+                                                'photo' => 'Aspandan.jpg',
+                                                'name' => 'Aspandan',
+                                                'where' => 'Escalation Manager, WIPRO Tech (Alumnus, MCA)',
+                                                'text_' => 'I was indeed blessed to study at Amrapali. The ambiance was very conductive for learning. The campus is very big & beautiful. The staff, teachers are very nice & supportive. The Technical support I got from my Professors helped me to compete with the actual scenario of the software industry. The guest lectures by visiting faculty members helped me to expand my knowledge horizon.'
+                                            ),
+                                            array(
+                                                'photo' => 'NiyatiShah.jpg',
+                                                'name' => 'Niyati Shah',
+                                                'where' => 'Associate Lead, Nagarro Inc (Alumnus, B.Tech.)',
+                                                'text_' => 'The time spent in Amrapali has been one of the most cherished days of my life. Not only were the faculty members cooperative but also the ones giving us opportunities to excel in various fields. The learnings in Amrapali helped me to communicate, innovate, and collaborate in ways that enabled me to build a successful career and stand-out in my career path. I wish all the Amrapalians good luck for all their future endeavors.'
+                                            ),
+                                            array(
+                                                'photo' => 'JaySinghDasila.jpg',
+                                                'name' => 'Jay Singh Dasila',
+                                                'where' => 'Asssociate Consultant, Capgemini India Pvt. Ltd. (Alumnus, B.Tech.)',
+                                                'text_' => 'I am working with Capgemini India Pvt. Ltd. as an Associate Consultant. I am a 2013 passout from Electronics & Communication Branch. I feel proud and obliged to be an alumnus of such an amazing Institute. The only thought which I follow and want to share is "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time". Students should also focus on their personality and communication skills in addition to their academics.'
+                                            ),
+                                            array(
+                                                'photo' => 'PoojaSinghal.jpg',
+                                                'name' => 'Pooja Singhal',
+                                                'where' => 'Senior Software Engineer, IBM (Alumnus, B.Tech.)',
+                                                'text_' => '"When I reflect my four years in Amrapali Institute for the B.Tech degree (2008-12), I conclude that the experience not only made me a better person but also prepared me for great career opportunities. I thoroughly enjoyed my time with incredibly supportive classmates and professors. It was a wonderful time in life, and the best educational experience Ive had."'
                                             )
                                         );
                                         ?>
@@ -69,12 +123,14 @@
                                                                     <div class="col-xs-12">
                                                                         <div class="portfolio-item">
                                                                             <div class="item-inner">
+                                                                                <blockquote>
                                                                                 <p align="justify"><img hspace="10" align="left" class="img-responsive img-thumbnail" style="margin-right:10px; max-width: 80px;" src="<?PHP echo base_url() . 'assets/img/alumni/' . $value['photo']; ?>" alt="Amrapali Alumni">
-                                                                                    <?php echo $value['text_']; ?>                                                                                    
+                                                                                    <?php echo $value['text_']; ?>
                                                                                 </p>                              
-                                                                                <h5 style="clear: both;margin-top:30px;">
+                                                                                <small>
                                                                                     <?php echo $value['name']; ?>, <?php echo $value['where']; ?>
-                                                                                </h5>
+                                                                                </small>
+                                                                            </blockquote>
                                                                             </div>
                                                                         </div>
                                                                     </div> 
