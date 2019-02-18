84b0ab09933e74bedabd81ba9e031edb2ab8468c
Dr. Nitin Deepak <nitin.d12@gmail.com>
2019-02-18 09:37:16 +0530

    updates
    updates

--- application/views/templates/socialMedia.php
+++ application/views/templates/socialMedia.php
@@ -4,6 +4,7 @@
 		<li><a href="https://twitter.com/agihld" title="Share on Twitter" target="_blank"><i class="fa fa-twitter" style="font-size:20px;"></i></a></li>
 		<li><a href="http://amrapali.ac.in/blog/" title="Share on Blog" target="_blank"><i class="fa fa-bold" style="font-size:20px;"></i></a></li>
 		<li><a href="https://www.instagram.com/agihld/" title="" target="_blank"><i class="fa fa-instagram" style="font-size:20px;"></i></a></li>
-		<li><a href="https://www.linkedin.com/in/amrapali-group-of-institutes-haldwani-08b26ab7/" target="_blank"><i class="fa fa-linkedin" style="font-size:20px;"></i></a></li>
+		<li><a href="https://www.linkedin.com/company/amrapali-group-of-institutes-haldwani" target="_blank"><i class="fa fa-linkedin" style="font-size:20px;"></i></a></li>
+		<!--li><a href="https://www.linkedin.com/in/amrapali-group-of-institutes-haldwani-08b26ab7/" target="_blank"><i class="fa fa-linkedin" style="font-size:20px;"></i></a></li-->
 		<li><a href="http://golive360.com/demo/amrpali05/all_files/virtual-tour/build.html" title="Virtual Tour, Amrapali Group of Institutes" target="_blank" style="color:#FBE54E"><i class="el"><img style="max-width: 45px;" hspace="0" vspace="0" src="<?PHP echo base_url() . 'assets/img/360.png' ?>" title="360" alt="Amrapali Group of Institute 360 degree view"></i></a></li>
 </ul>
